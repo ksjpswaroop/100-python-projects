449475c8825494f903d28cb0a2374ff1c1258f21
Evangelos Pragalakis <vagp21@hotmail.com>
2017-05-28 14:13:49 +0300

    numbers file added

---
+++ numbers/next-prime-numbers.py
@@ -0,0 +1,21 @@
+import math
+def is_prime():
+    x = [2]
+    for value in range(3,1000):
+        if value % 2 == 0 and value > 2:
+            continue
+        if all(value % i for i in range(3, int(math.sqrt(value)) + 1, 2)):
+            x.append(value)
+    return x
+
+
+def msg():
+    x = is_prime()
+    for value in range(len(x)):
+        inpt = raw_input("Show more prime numbers? Y/N: ")
+        if inpt == 'N' or inpt == 'n':
+            return 0
+        elif inpt == 'Y' or inpt == 'y':
+            print x[value]
+
+msg()
