b389c54d137b02d4d1023ddd508c01fe5265673b
Evangelos Pragalakis <vagp21@hotmail.com>
2017-06-09 18:56:55 +0300

    algorithm file added

--- classic-agorithms/collatz-conjecture.py
+++ classic-agorithms/collatz-conjecture.py
@@ -1,19 +1,19 @@
 
-def colalg(n):
-  step = [1]
+def colalg(n,step):
   if n < 0:
       return 0
   elif n == 1:
       return sum(step)
   elif n % 2 == 0:
       step.append(1)
-      return colalg(n/2)
+      return colalg(n/2,step)
   else:
       step.append(1)
-      return colalg(n * 3 + 1)
+      return colalg(n * 3 + 1,step)
 
 
 
 
 inpt = input("Number: ")
-print(colalg(int(inpt)))
+step = [1]
+print(colalg(int(inpt),step))
