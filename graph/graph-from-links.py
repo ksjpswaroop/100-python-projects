652a2563aa06c4981f132ed3544c0f00b6ce26a2
Evangelos Pragalakis <vagp21@hotmail.com>
2017-06-16 19:22:48 +0300

    test

--- graph/graph-from-links.py
+++ graph/graph-from-links.py
@@ -1,1 +1,16 @@
+def graph(link):
+    node = {}
+    for i in range(0,len(link)):
+        #if i not in node[i]:
+        #node[i].append(link[i][1])
+        #node.insert(i,link[i][0])
+        #else:
+        #    node.insert(i,link[i])
+        node[link[i][0]] = link[i][1]
+        print(node)
 
+
+
+
+link = [[1,2],[2,3],[2,4],[3,4]]
+graph(link)
