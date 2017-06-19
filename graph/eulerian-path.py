4053975736872e7fa876924496376779d08de542
Evangelos Pragalakis <vagp21@hotmail.com>
2017-06-19 21:53:23 +0300

    Update eulerian-path.py

--- graph/eulerian-path.py
+++ graph/eulerian-path.py
@@ -10,13 +10,17 @@ def eulerian(graph):
     for key,val in final.items():
         if (len(val)) % 2 != 0:
             odd.append(1)
-    if sum(odd) == 0 or sum(odd) == 2:
+    if sum(odd) == 2:
         print("Eulerian path")
+    elif sum(odd) == 0 :
+        print("Eulerian circuit")
     else:
         print("Not an eulerian path")
 
 
 link_1 = [['A','B'],['B','C'],['B','D'],['C','D'],['D','A']]
 link_2 = [['A','B'],['B','C'],['B','D'],['C','D']]
+link_3 = [['A','B'],['A','C'],['B','C'],['B','D'],['C','D'],['C','A']]
 eulerian(link_1)
 eulerian(link_2)
+eulerian(link_3)
