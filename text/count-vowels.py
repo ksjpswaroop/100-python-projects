8507c5ea8a593a27c69bfcb518059da4d8898722
Evangelos Pragalakis <11145139+pragalakis@users.noreply.github.com>
2018-03-27 19:18:24 +0300

    Update count-vowels.py

--- text/count-vowels.py
+++ text/count-vowels.py
@@ -1,8 +1,11 @@
 def count_vowels(string):
     vowels = {'a':0,'e':0,'i':0,'o':0,'u':0,'y':0}
+    sumv = 0
     for i in range(0,len(string)):
         if string[i] in vowels.keys():
             vowels[string[i]]+=1
+            sumv+=1
+    print('Number of total vowels: ' + str(sumv))
     return vowels
 
 string = input('Give string: ')
