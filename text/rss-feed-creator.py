75f5e4b0c75a5198de578cadf0f04b0f38398e98
Evangelos Pragalakis <11145139+pragalakis@users.noreply.github.com>
2018-04-02 18:00:17 +0300

    Update rss-feed-creator.py

--- text/rss-feed-creator.py
+++ text/rss-feed-creator.py
@@ -3,7 +3,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 headers = { 'User-Agent' : 'Mozilla/5.0' }
-URL = "https://medialibre.net/feed/atom/"
+URL = "given url"
 page = urllib.request.Request(URL, None, headers)
 page = urllib.request.urlopen(page)
 data = page.read()
@@ -12,11 +12,22 @@ page.close()
 def parse(data):
     soup = BeautifulSoup(data, features='xml')
     entries = soup.find_all('entry')
-    for entry in entries:
-        print(entry.title.text)
-        print(entry.id.text)
-        print('\n')
-        if (entry.summary.text):
-            print(entry.summary.text + '\n')
+    items = soup.find_all('item')
+    if entries:
+        for entry in entries:
+            print(entry.title.text)
+            print(entry.link.get('href'))
+            print('\n')
+            if entry.summary:
+                print(entry.summary.text + '\n')
+            elif entry.content:
+                print(entry.content.text + '\n')
+    elif items:
+        for item in items:
+            print(item.title.text)
+            print(item.link.text)
+            print('\n')
+            if item.description:
+                print(item.description.text + '\n')
 
 parse(data)
