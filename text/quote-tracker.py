a9630aad09735aed587c3638a43e58064d289072
Evangelos Pragalakis <11145139+pragalakis@users.noreply.github.com>
2018-04-03 15:29:17 +0300

    Create quote-tracker.py

---
+++ text/quote-tracker.py
@@ -0,0 +1,38 @@
+'''
+Quote Tracker (crypto symbols etc)
+A program which can go out and check the current value of cryptocurrencies for a list of symbols entered by the user.
+The user can set how often the cryptocurrencies are checked. For CLI, show whether the cryptocurrency has moved up or down.
+(using the coinmarketcap api)
+'''
+
+import urllib.request
+import json
+
+def cryptoprices(sym):
+    #coinmarketcap api link, works for 100 cryptocurrencies (add limit for more)
+    ticker = "https://api.coinmarketcap.com/v1/ticker/"
+    prices = {}
+
+    headers = { 'User-Agent' : 'Mozilla/5.0' }
+    page = urllib.request.Request(ticker, None, headers)
+    page = urllib.request.urlopen(page)
+    data = page.read()
+    encoding = page.info().get_content_charset('utf-8')
+    data = json.loads(data.decode(encoding))
+    page.close()
+
+    for x in data:
+        for i in range(0,len(sym)):
+            if x['symbol']==sym[i]:
+                print(sym[i])
+                prices[sym[i]] = [x['price_usd']]
+                #prices[sym[i]].append(x['price_usd'])
+                sym.remove(sym[i])
+                break
+
+    print(prices)
+    print('Not found: ' + str(sym))
+
+
+inpt = ['BTC','LTC','TEST']
+cryptoprices(inpt)
