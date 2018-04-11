6e0d7a6833ee92b005bb034eac90b30478af1731
Evangelos Pragalakis <11145139+pragalakis@users.noreply.github.com>
2018-04-11 20:25:13 +0300

    Create port-scanner.py

---
+++ networking/port-scanner.py
@@ -0,0 +1,57 @@
+'''
+Port Scanner
+Enter an IP address and a port range where the program will then attempt
+to find open ports on the given computer by connecting to each of them.
+On any successful connections mark the port as open.
+'''
+import socket
+
+
+def port_scanner(ip, port_range):
+    port_range = port_range.split("-")
+
+    try:
+        for port in range(int(port_range[0]), int(port_range[1])):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((ip, port))
+            if result == 0:
+                print("Port {}: Open".format(port))
+                sock.close()
+
+    except socket.gaierror:
+        print("Hostname could not be resolved.")
+        raise SystemExit
+
+    except socket.error:
+        print("Couldn't connect to server")
+        raise SystemExit
+
+
+def valid_ip(ip):
+    try:
+        socket.inet_aton(ip)
+        return True
+
+    except socket.error:
+        print("Not a valid ip.")
+        raise SystemExit
+
+
+def valid_range(port_range):
+    port_range = port_range.split("-")
+    valid_range = range(1, 1025)
+
+    if port_range[0] >= port_range[1]:
+        print("error range")
+        return False
+
+    if port_range[0] and port_range[1] in valid_range:
+        return True
+        
+
+#console
+port_range = input("Port range: ")
+ip = input("IP: ")
+
+if valid_ip(ip) and valid_range(port_range):
+    port_scanner(ip, port_range)
