29bfcda9ce969e293e415ce72bd64486f53dab6d
Evangelos Pragalakis <11145139+pragalakis@users.noreply.github.com>
2018-04-18 22:00:48 +0300

    Update recipe-manager.py

--- classes/recipe-manager.py
+++ classes/recipe-manager.py
@@ -12,3 +12,32 @@ class Recipe(object):
 
     def get_ingredients(self):
         return self.ingredients
+
+
+class Manager(object):
+
+    def __init__(self):
+        self.recipe_list = {}
+        self.deserts = {}
+        self.main_courses = {}
+
+    def add_recipe(self, name, recipe, type):
+        self.recipe_list[name] = recipe
+
+        if type == "Main course":
+            self.main_courses[name] = recipe
+
+        elif type == "Desert":
+            self.deserts[name] = recipe
+
+    def show_recipes(self):
+        for k, l in self.recipe_list.items():
+            print(k + ": " + str(l))
+
+    def show_deserts(self):
+        for k, l in self.deserts.items():
+            print(k + ": " + str(l))
+
+    def show_main_courses(self):
+        for k, l in self.main_courses.items():
+            print(k + ": " + str(l))
