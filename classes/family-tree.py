83f788a1ea490e80a185ef93df7303ddb5316453
Evangelos Pragalakis <11145139+pragalakis@users.noreply.github.com>
2018-04-20 16:40:45 +0300

    Update family-tree.py

--- classes/family-tree.py
+++ classes/family-tree.py
@@ -1,32 +1,65 @@
-'''
-Family Tree Creator
-Create a class called Person which will have a name, when they were born and when (and if) they died. Allow the user to create these Person classes and put them into a family tree structure. Print out the tree to the screen.
-'''
-
 class Person(object):
 
-    def __init__(self, fname, lname, born, died=None):
+    def __init__(self, fname, lname, born=None, died=None):
         self.fname = fname
         self.lname = lname
         self.born = born
         self.died = died
-        self.mother = ""
-        self.father = ""
-        self.grandmother = ""
-        self.granddad = ""
-        self.siblings_list = []
+        self.siblings_list = None
 
-    def parents(self, mother, father):
+    def parents(self, mother=None, father=None):
         self.mother = mother
         self.father = father
 
     def siblings(self, *args):
+        self.siblings_list = []
         for arg in args:
             self.siblings_list.append(arg)
 
-    def grandparents(self, granddad, grandmother):
+    def grandparents(self, granddad=None, grandmother=None):
         self.granddad = granddad
         self.grandmother = grandmother
 
-    def show_members(self):
-        print(self.fname + " " + self.lname, self.mother, self.father, self.grandmother, self.granddad, self.siblings_list)
+
+    def get_members(self):
+        family_members = {"First name": self.fname,
+                          "Last name" : self.lname,
+                          "Grandfather": self.granddad,
+                          "Grandmother": self.grandmother,
+                          "Father": self.father,
+                          "Mother": self.mother,
+                          "Siblings": self.siblings_list
+                         }
+
+        return family_members
+
+
+class Relations(object):
+
+    def __init__(self):
+        self.child = {}
+        self.partners = {}
+
+    def get_child(self, person):
+
+        #for grandfathers and grandmothers childs
+        #supposedly the child got its father's last name
+        if person["Last name"] in person["Father"]:
+            self.child[person["Grandfather"]] = person["Father"]
+            self.child[person["Grandmother"]] = person["Father"]
+
+        elif person["Last name"] in person["Mother"]:
+            self.child[person["Grandfather"]] = person["Mother"]
+            self.child[person["Grandmother"]] = person["Mother"]
+
+        #for parents childs
+        self.child[person["Mother"]] = person["Siblings"]
+        self.child[person["Father"]] = person["Siblings"]
+        return self.child
+
+
+    def get_partners(self, person):
+        self.partners[person["Grandfather"]] = person["Grandmother"]
+        self.partners[person["Father"]] = person["Mother"]
+
+        return self.partners
