4b7dd8af42c61b8f7fef9bc25489db787c33f17e
Evangelos Pragalakis <11145139+pragalakis@users.noreply.github.com>
2018-04-26 00:54:15 +0300

    Update bulk-thumbnail-creator.py

--- threading/bulk-thumbnail-creator.py
+++ threading/bulk-thumbnail-creator.py
@@ -3,35 +3,48 @@ Bulk Thumbnail Creator
 Picture processing can take a bit of time for some transformations. Especially if the image is large. Create an image program which can take hundreds of images and converts them to a specified size in the background thread while you do other things. For added complexity, have one thread handling re-sizing, have another bulk renaming of thumbnails etc.
 '''
 import multiprocessing
-import os, sys
+import os, time
 from PIL import Image
 
 
-def resize(img_list):
-    size = 250, 250
+def resize(img_list, path_img, path_thumb, size):
+    print(multiprocessing.current_process())
 
     for img in img_list:
-        #path of images
-        infile = "images/" + img
-        #path of thumbnails
-        outfile = "images/thumbnail/" + img
+        #image thumb
+        infile = path_img + img
+        #thumbnail puth
+        outfile = path_thumb + img
+
         try:
             im = Image.open(infile)
             im.thumbnail(size, Image.ANTIALIAS)
             im.save(outfile, "JPEG")
+
         except IOError:
-            print("cannot create thumbnail for image {}".format(img))
+            print("Cannot create thumbnail for image {}".format(img))
 
 
-def rename(img_list):
+def rename(img_list, path_thumb):
+    print(multiprocessing.current_process())
+
     for img in img_list:
-        path = "images/thumbnail/"
-        old_file = os.path.join(path, img)
-        new_file = os.path.join(path, "thumb-" + img)
+        old_file = os.path.join(path_thumb, img)
+        new_file = os.path.join(path_thumb, "thumb-" + img)
         os.rename(old_file, new_file)
 
 
 img_list = [str(x) for x in range(1,34)]
+size = 250, 250
+path_img = "images/"
+path_thumb = "images/thumbnail/"
 
-#resize(img_list)
-#rename(img_list)
+p1 = multiprocessing.Process(target = resize, args = [img_list, path_img, path_thumb,size,])
+p2 = multiprocessing.Process(target = rename, args = [img_list, path_thumb,])
+
+p1.start()
+time.sleep(2)
+p2.start()
+
+p1.join()
+p2.join()
